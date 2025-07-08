d5e269b1dce805694866dbdaf7409f6dbd6c291a
Synkoo <adoncel2002@gmail.com>
2025-07-08 19:55:44 +0200

    Feature: primera traduccion castellano

--- localization/es_ES.lua
+++ localization/es_ES.lua
@@ -6,12 +6,82 @@ return {
         Enhanced={},
         Joker={   
             j_ina_markevansboy = {
-            name = "Mark Evans",
-            text = {
-                "{C:chips}+#1#{} Chips",
-            } 
-        },},
-        Other={},
+                name = "Mark Evans Joven",
+                text = {
+                    "¡Nunca te rindas!",
+                    "{C:mult}+#1#{} de Mult en tu última mano",
+                    "Aumenta en {C:mult}+#2#{} por cada última mano jugada",
+                    "{C:inactive}(Evoluciona con {C:mult}+#1#{C:inactive} / {C:mult}+#3#{C:inactive} de Mult)",
+                } 
+            },
+            j_ina_markevansschool = {
+                name = "Mark Evans Escolar",
+                text = {
+                    "{C:gk}¡Nunca te rindas!+",
+                    "{C:mult}+#1#{} de Mult en tu última mano",
+                    "Aumenta en {C:mult}+#2#{} por cada última mano jugada",
+                    "{C:inactive}(Evoluciona con {C:mult}+#1#{C:inactive} / {C:mult}+#3#{C:inactive} de Mult)",
+                } 
+            },
+            j_ina_markevansadult = {
+                name = "Mark Evans Adulto",
+                text = {
+                    "{C:gk}¡Nunca te rindas!++",
+                    "{X:mult,C:white}X#1#{} de Mult en tu última mano",
+                    "Aumenta en {X:mult,C:white}X#2#{} por cada última mano jugada",
+                } 
+            },
+            j_ina_axelblazehooded = {
+                name = "Encapuchado",
+                text = {
+                    "{C:mult}+#1#{} de Mult en tu última mano",
+                } 
+            },
+            j_ina_axelblazeschooler = {
+                name = "Axel Blaze Escolar",
+                text = {
+                    "{C:gk}¡Nunca te rindas!++",
+                    "{C:mult}+#1#{} de Mult en tu última mano",
+                    "Aumenta en {C:mult}+#2#{} por cada última mano jugada",
+                    "{C:inactive}(Evoluciona con {C:mult}+#1#{C:inactive} / {C:mult}+#3#{C:inactive} de Mult)",
+                } 
+            },
+            j_ina_axelblazeadult = {
+                name = "Axel Blaze Adulto",
+                text = {
+                    "{C:gk}¡Nunca te rindas!++",
+                    "{C:mult}+#1#{} de Mult en tu última mano",
+                    "Aumenta en {C:mult}+#2#{} por cada última mano jugada",
+                    "{C:inactive}(Evoluciona con {C:mult}+#1#{C:inactive} / {C:mult}+#3#{C:inactive} de Mult)",
+                } 
+            },
+        },
+        Other={
+            Mountain = {
+                name = "Tipo",
+                text = {
+                  "{X:mountain,C:white}Montaña{}",
+                }
+            },
+            Fire = {
+                name = "Tipo",
+                text = {
+                  "{X:fire,C:white}Fuego{}",
+                }
+            },
+            GK = {
+                name = "Posición",
+                text = {
+                  "{X:gk,C:white}Portero{}",
+                }
+            },
+            FW = {
+                name = "Posición",
+                text = {
+                  "{X:fw,C:white}Delantero{}",
+                }
+            },
+        },
         Planet={},
         Spectral={},
         Stake={},
@@ -25,7 +95,10 @@ return {
         blind_states={},
         challenge_names={},
         collabs={},
-        dictionary={},
+        dictionary={
+            ina_evolve_level = "¡Sube de nivel!",
+            ina_evolve_success = "¡Ha crecido!",
+        },
         high_scores={},
         labels={},
         poker_hand_descriptions={},
