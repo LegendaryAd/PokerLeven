6be1500b2cb29e8a59828b5714225cab58e56f4f
Synkoo <adoncel2002@gmail.com>
2025-07-26 14:09:03 +0200

    Feature: Hayseed

--- players/farm.lua
+++ players/farm.lua
@@ -15,20 +15,23 @@ local Greeny = J({
     pposition = "GK",
     pteam = "Farm",
     blueprint_compat = true,
-    calculate = function(self, card, context)
-        -- Add logic
-    end,
 })
+
 
 local Hayseed = J({
     name = "Hayseed",
     pos = { x = 11, y = 4 },
-    config = {},
+    config = {
+        extra = {
+            current_mult = 0,
+            mult_mod = 2
+        }
+    },
     loc_vars = function(self, info_queue, center)
         type_tooltip(self, info_queue, center)
-        return {}
+        return { vars = { center.ability.extra.current_mult or 0 } }
     end,
-    rarity = 1, -- Common
+    rarity = 1,
     pools = { ["Farm"] = true },
     cost = 8,
     atlas = "Jokers01",
@@ -37,9 +40,25 @@ local Hayseed = J({
     pteam = "Farm",
     blueprint_compat = true,
     calculate = function(self, card, context)
-        -- Add logic
+        if context.cardarea == G.jokers and context.scoring_hand and context.joker_main
+            and card.ability.extra.current_mult > 0 then
+            return {
+                message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.current_mult } },
+                mult_mod = card.ability.extra.current_mult,
+                colour = G.C.MULT
+            }
+        end
+        if context.before and context.cardarea == G.jokers and context.scoring_hand
+            and next(context.poker_hands["Three of a Kind"]) and #context.scoring_hand == 3 then
+            card.ability.extra.current_mult = card.ability.extra.current_mult + card.ability.extra.mult_mod
+            return {
+                message = localize("k_upgrade_ex"),
+                colour = G.C.MULT
+            }
+        end
     end,
 })
+
 
 local Sherman = J({
     name = "Sherman",
