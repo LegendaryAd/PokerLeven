7f093556f9828607a62ebb05b97c734bb472d6f7
Synkoo <adoncel2002@gmail.com>
2025-07-30 12:49:48 +0200

    Fix: Apollo changes 2

--- players/zeus.lua
+++ players/zeus.lua
@@ -80,6 +80,9 @@ local Apollo = {
         mod = card.ability.extra.chips_mod
       end
 
+      local two_pairs_count = #context.poker_hands["Two Pair"]
+      local added_bonus = two_pairs_count * mod
+
       card.ability.extra.current_bonus = card.ability.extra.current_bonus + added_bonus
 
       return {
