5b47f580159855f6cf46d9b891f9c163561826cd
Shadorossa <113119429+Shadorossa@users.noreply.github.com>
2025-08-25 18:46:20 +0200

    Feature: All Gemini's arc players

---
+++ players/IE2_E_Epsilon.lua
@@ -0,0 +1,224 @@
+-- Dvalin
+local Dvalin = {
+  name = "Dvalin",
+  pos = { x = 0, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Forest,
+  pposition = C.GK,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+-- Kenville
+local Kenville = {
+  name = "Kenville",
+  pos = { x = 1, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Wind,
+  pposition = C.DF,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+-- Mole
+local Mole = {
+  name = "Mole",
+  pos = { x = 2, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Forest,
+  pposition = C.DF,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+-- Kayson
+local Kayson = {
+  name = "Kayson",
+  pos = { x = 3, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Wind,
+  pposition = C.DF,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+-- Tytan
+local Tytan = {
+  name = "Tytan",
+  pos = { x = 4, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Mountain,
+  pposition = C.DF,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+-- Fedora
+local Fedora = {
+  name = "Fedora",
+  pos = { x = 5, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Fire,
+  pposition = C.MF,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+-- Krypto
+local Krypto = {
+  name = "Krypto",
+  pos = { x = 6, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Wind,
+  pposition = C.MF,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+-- Sworm
+local Sworm = {
+  name = "Sworm",
+  pos = { x = 7, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Forest,
+  pposition = C.MF,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+-- Mercury
+local Mercury = {
+  name = "Mercury",
+  pos = { x = 8, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Wind,
+  pposition = C.FW,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+-- Metron
+local Metron = {
+  name = "Metron",
+  pos = { x = 9, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Wind,
+  pposition = C.FW,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+-- Zell
+local Zell = {
+  name = "Zell",
+  pos = { x = 10, y = 5 },
+  config = { extra = {} },
+  loc_vars = function(self, info_queue, center)
+    return {}
+  end,
+  rarity = 1, -- Common
+  pools = { ["Epsilon"] = true },
+  cost = 7,
+  atlas = "Jokers02",
+  ptype = C.Fire,
+  pposition = C.FW,
+  pteam = "Epsilon",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+  end
+}
+
+return {
+  name = "Tormenta de Géminis",
+  list = { Dvalin, Kenville, Mole, Kayson, Tytan, Fedora, Krypto, Sworm, Mercury, Metron, Zell },
+}
