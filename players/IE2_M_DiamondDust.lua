fbcf40f15f7375f2cebf7567c5897273241de130
Shadorossa <113119429+Shadorossa@users.noreply.github.com>
2025-08-28 01:57:54 +0200

    Update top

--- players/IE2_M_DiamondDust.lua
+++ players/IE2_M_DiamondDust.lua
@@ -1,17 +1,61 @@
--- Gazelle
-local Zell = {
-    name = "Zell",
-    pos = { x = 10, y = 5 },
+-- Beluga
+local Beluga = {
+    name = "Beluga",
+    pos = { x = 6, y = 14 },
     config = { extra = {} },
     loc_vars = function(self, info_queue, center)
         return {}
     end,
-    rarity = 1, -- Common
+    rarity = 1, --
     pools = { ["DiamondDust"] = true },
     cost = 7,
     atlas = "Jokers02",
     ptype = C.Wind,
+    pposition = C.MF,
+    techtype = C.UPGRADES.Plus,
+    pteam = "Polvo de Diamantes",
+    blueprint_compat = true,
+    calculate = function(self, card, context)
+    end
+}
+
+-- Icer
+local Icer = {
+    name = "Icer",
+    pos = { x = 10, y = 14 },
+    config = { extra = {} },
+    loc_vars = function(self, info_queue, center)
+        return {}
+    end,
+    rarity = 1, --
+    pools = { ["DiamondDust"] = true },
+    cost = 7,
+    atlas = "Jokers02",
+    ptype = C.Wind,
+    pposition = C.MF,
+    techtype = C.UPGRADES.Plus,
+    pteam = "Polvo de Diamantes",
+    blueprint_compat = true,
+    calculate = function(self, card, context)
+    end
+}
+
+-- Gazelle
+local Gazelle = {
+    name = "Gazelle",
+    pos = { x = 7, y = 0 },
+    soul_pos = { x = 7, y = 1 },
+    config = { extra = {} },
+    loc_vars = function(self, info_queue, center)
+        return {}
+    end,
+    rarity = "ina_top", -- Destacado
+    pools = { ["DiamondDust"] = true },
+    cost = 7,
+    atlas = "top",
+    ptype = C.Wind,
     pposition = C.FW,
+    techtype = C.UPGRADES.Grade,
     pteam = "Polvo de Diamantes",
     blueprint_compat = true,
     calculate = function(self, card, context)
@@ -20,6 +64,6 @@ local Zell = {
 
 return {
     name = "Diamond Dust",
-    list = {}
+    list = { Beluga, Icer, Gazelle }
     -- list = { Dvalin, Kenville, Mole, Kayson, Tytan, Fedora, Krypto, Sworm, Mercury, Metron, Zell },
 }
